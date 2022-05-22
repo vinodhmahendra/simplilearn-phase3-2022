7cbd77ed025aea1d297de1fd070e101b97a49410
Vinodh Mahendra <vinodh.mahendra@gmail.com>
2022-05-22 20:39:30 +0530

    spring boot hello world application

--- 09-hello-world-spring-boot/src/main/java/com/simplilearn/workshop/Application.java
+++ 09-hello-world-spring-boot/src/main/java/com/simplilearn/workshop/Application.java
@@ -1,5 +1,6 @@
 package com.simplilearn.workshop;
 
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.setBannerMode(Mode.OFF);
+		application.run(args);
 	}
 
 }
