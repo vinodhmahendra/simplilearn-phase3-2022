93622eec9651c6c52e67b63b9e01fce567456f35
Vinodh Mahendra <vinodh.mahendra@gmail.com>
2022-05-29 21:04:45 +0530

    spring boot restful api server and jpa h2

---
+++ 14-todo-service-rest-jpa-h2/src/main/java/com/simplilearn/workshop/service/TodoServiceImpl.java
@@ -0,0 +1,58 @@
+package com.simplilearn.workshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.simplilearn.workshop.domain.Todo;
+import com.simplilearn.workshop.repository.TodoRepository;
+
+@Service(value = "todoService")
+public class TodoServiceImpl implements TodoService{
+	
+	//depends on 'TodoRepository' 
+//	private TodoRepository todoRepository = new TodoRepositoryImpl(); //old code
+	
+	@Autowired
+	private TodoRepository todoRepository; //property name
+	
+	
+	//spring default constructor
+	public TodoServiceImpl() {
+		
+	}
+	
+	public TodoServiceImpl(TodoRepository todoRepository) {
+		super();
+		System.out.println("spring container called constructor to  assemble 'todoRepository' bean");
+		this.todoRepository = todoRepository;
+	}
+
+	public void setTodoRepository(TodoRepository todoRepository) {
+		System.out.println("spring container called setter method to  assemble 'todoRepository' bean");
+		this.todoRepository = todoRepository;
+	}
+
+	@Override
+	public List<Todo> findAll() {
+		return todoRepository.findAll();
+	}
+
+	@Override
+	public Todo save(Todo theTodo) {
+		return todoRepository.save(theTodo);
+	}
+
+	@Override
+	public Todo deleteById(long theId) {
+		 todoRepository.deleteById(theId);
+		 return null;
+	}
+
+	@Override
+	public Todo findById(long theId) {
+		return todoRepository.findById(theId).get();
+	}
+
+}
